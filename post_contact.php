aa3759eafccb5bc606c24f0fabd22ce8d38356d5
Zo <zoe.marday@outlook.com>
2020-09-22 23:39:18 +0200

    Update post_contact.php

--- post_contact.php
+++ post_contact.php
@@ -5,8 +5,8 @@
         $errors['name'] = "Vous n'avez pas renseigné votre nom";
     }
 
-    if(!array_key_exists('email', $_POST) || $_POST['email'] == ''){
-        $errors['email'] = "Vous n'avez pas renseigné votre email";
+    if(!array_key_exists('email', $_POST) || $_POST['email'] == '' || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        $errors['email'] = "Vous n'avez pas renseigné un email valide";
     }
 
     if(!array_key_exists('message', $_POST) || $_POST['message'] == ''){
@@ -16,10 +16,13 @@
     if(!empty($errors)){
         session_start();
         $_SESSION['errors'] = $errors;
+        $_SESSION['inputs'] = $_POST;
         header('Location: index.php');
     } else{
+        $_SESSION['success'] = 1;
         $message = $_POST['message'];
         $headers = 'FROM: site@local.dev';
         mail('zwolf@outlook.fr', 'Formulaire de contact', $message, $headers);
+        header('Location: index.php');
     }
 ?>
