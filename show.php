9816ee2f52d302cbc0e22cb3e291fff8b8357d3e
Zo <zoe.marday@outlook.com>
2020-09-22 23:45:33 +0200

    Update show.php

---
+++ show.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.min.css">
+  <title>Alexis Dupont</title>
+</head>
+<body> 
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+   <div class="container">
+      <a class="navbar-brand" href="list.php">Historique E-mail</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav ml-auto">  
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Écrire un e-mail</a>
+          </li> 
+        </ul> 
+      </div>
+   </div>
+</nav>  <div class="container">
+    <div class="row mt-4">
+      <div class="col-8 offset-2"> 
+        <div class="card">
+          <div class="card-header">
+            <div class="float-right">
+              john@email.com            </div>
+            <h1 class="h4">John Doe</h1>
+          </div>
+          <div class="card-body">
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+
+          </div>
+          <div class="card-footer">
+            <div class="float-right">
+              <a href="functions/deleteMessage.php?id=5" class="btn btn-danger btn-sm">Supprimer</a>
+            </div>
+            2020-09-21 16:49:46          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
