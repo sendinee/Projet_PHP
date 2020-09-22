397b930ff45c5eff81270704e983069e8916704d
Zo <zoe.marday@outlook.com>
2020-09-22 20:43:39 +0200

    Add files via upload

---
+++ list.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de contact</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="list.php">Historique E-mail</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Écrire un e-mail</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>  <div class="container">
+    <div class="row mt-4">
+        <div class="col-6 offset-3">
+            <div class="card mb-2 ">
+                <a href="show.php?id=8" class="card-body text-decoration-none">
+                    <div class="float-right text-sm text-muted">
+                        2020-09-22 15:26:49                          </div>
+                    <h3>Sendinee MARDAY</h3>
+                    <h6 class="text-muted">s.marday@gmail.com</h6>
+                </a>
+            </div>
+            <div class="card mb-2 ">
+                <a href="show.php?id=5" class="card-body text-decoration-none">
+                    <div class="float-right text-sm text-muted">
+                        2020-09-21 16:49:46                          </div>
+                    <h3>Joe LETAXI</h3>
+                    <h6 class="text-muted">john.letaxi75@outlook.com</h6>
+                </a>
+            </div>
+            <div class="card mb-2 ">
+                <a href="show.php?id=4" class="card-body text-decoration-none">
+                    <div class="float-right text-sm text-muted">
+                        2020-09-21 16:47:03                          </div>
+                    <h3>Sadrine Phan</h3>
+                    <h6 class="text-muted">s.phan@yahoo.com</h6>
+                </a>
+            </div>
+            <div class="card mb-2 ">
+                <a href="show.php?id=3" class="card-body text-decoration-none">
+                    <div class="float-right text-sm text-muted">
+                        2020-09-21 16:46:36                          </div>
+                    <h3>Sabrina SOPHIE</h3>
+                    <h6 class="text-muted">s.sophie@hotmail.com</h6>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
